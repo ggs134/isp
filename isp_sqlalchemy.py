ce846b2e898db571b0a171e8b3ca57894ab981df
KIMHWIKYUNG <hwi@KIMHWIKYUNGui-MacBook-Air.local>
2015-11-09 16:16:47 +0900

    isp_sqlalchemy.py 추가2

--- isp_sqlalchemy.py
+++ isp_sqlalchemy.py
@@ -1,13 +1,15 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, Unicode, String
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship, backref
 
-Base = delclarative_base()
+Base = declarative_base()
 
 
 class Organization(Base):
   __tablename__ = 'organization'
-  organization_num = column(Integer, primary_key=True)
+  organization_num = Column(Integer, primary_key=True)
   supraorganizaion_name = Column(String(50))
   subedifice_name = Column(String(50))
   o_g_n = relationship("Organization_goal", backref="organization")
@@ -15,7 +17,7 @@ class Organization(Base):
 
 class Goal(Base):
   __tablename__ = 'goal'
-  goal_num = column(Integer, primary_key=True)
+  goal_num = Column(Integer, primary_key=True)
   originalgoal_name = Column(String(50))
   subgoal_name = Column(String(50))
   g_n = relationship("Organization_goal", backref="goal")
@@ -23,6 +25,9 @@ class Goal(Base):
 
 class Organization_goal(Base):
   __tablename__ = 'organization_goal'
-  organization_num = column(Integer, ForeignKey('organization.organization_num'))
-  goal_num = column(Integer, ForeignKey('goal.goal_num'))
+  organization_num = Column(Integer, ForeignKey('organization.organization_num'))
+  goal_num = Column(Integer, ForeignKey('goal.goal_num'))
   importance = Column(String(2))
+
+engine = create_engine('mysql://root:1127@localhost/isptest', encoding='utf8', echo=True)
+Base.metadata.create_all(engine)
