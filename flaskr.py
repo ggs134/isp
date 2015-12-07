ae52d740f1b728d96fcd87d8b5c4487d2d49c365
ggs134 <inditow@gmail.com>
2015-12-07 15:56:15 +0900

    PUT methods to Post about Update

--- flaskr.py
+++ flaskr.py
@@ -149,18 +149,6 @@ def show_object():
 			session.close()
 			return jsonify(results = "1")
 
-		elif type(idf) == type("") :
-		  	return jsonify(results = "0")
-
-
-	elif request.method == "PUT":
-	 	#Get Request
-	 	data = request.get_json(force=True)
-	 	#get data from requested
-	 	idf = data['a'].encode('utf-8')
-	 	obj_code = data['obj_code'].encode('utf-8')
-	 	obj_desc = data['obj_desc'].encode('utf-8')
-	 	obj_priority = data['obj_priority'].encode('utf-8')
 
 		if idf == 'UpdateObject':
 			#데이터베이스에서 업데이트 하고자 하는 객체를 불러옴
@@ -179,6 +167,34 @@ def show_object():
 
 		elif type(idf) == type("") :
 		  	return jsonify(results = "0")
+
+
+	# elif request.method == "PUT":
+	#  	#Get Request
+	#  	data = request.get_json(force=True)
+	#  	#get data from requested
+	#  	idf = data['a'].encode('utf-8')
+	#  	obj_code = data['obj_code'].encode('utf-8')
+	#  	obj_desc = data['obj_desc'].encode('utf-8')
+	#  	obj_priority = data['obj_priority'].encode('utf-8')
+
+	# 	if idf == 'UpdateObject':
+	# 		#데이터베이스에서 업데이트 하고자 하는 객체를 불러옴
+	# 		query = session.query(Object)
+	# 	  	query_row = query.from_statement("select * from object where obj_code=:obj_code").params(obj_code=obj_code).first()
+	# 		#만약 객체가 조회되지 않을 경우 0을 반환
+	# 		if query_row is None :
+	# 			return jsonify(results = "0")
+	# 		#조회된 객체안에 컬럼에 접근하여 사용자가 요청한 데이터를 입력해줌
+	# 		query_row.obj_desc = obj_desc
+	# 		query_row.obj_priority = obj_priority
+	# 		#세션 커밋 믿 닫음
+	# 		session.commit()
+	# 		session.close()
+	# 		return jsonify(results = "1")
+
+	# 	elif type(idf) == type("") :
+	# 	  	return jsonify(results = "0")
 
 	elif request.method == "DELETE":
 	 	#Get Request
@@ -240,9 +256,22 @@ def show_department():
 		  		return jsonify(results = converted_list)
 		  	else:
 		  		return jsonify(results = "0")
+		
 		elif idf == 'CreateDepartment':
 			newDept=Department(dept_code = dept_code, dept_desc = dept_desc)
 			session.add(newDept)
+			session.commit()
+			session.close()
+			return jsonify(results = "1")
+
+		if idf =='UpdateDepartment':
+			query = session.query(Department)
+			query_updated = query.from_statement("select * from department where dept_code=:dept_code").params(dept_code=dept_code).first() ##
+			if query_updated is None:
+				return jsonify(results="0")
+			query_updated.dept_code = dept_code
+			query_updated.dept_desc = dept_desc
+			# 세션 커밋
 			session.commit()
 			session.close()
 			return jsonify(results = "1")
@@ -273,26 +302,26 @@ def show_department():
 	# 		return jsonify(results = "1")
 
 
-	# 삭제 수행
-	elif request.method== 'PUT' :
-		data = request.get_json(force=True)
-		idf = data['a'].encode('utf-8')
-		dept_code = data['dept_code'].encode('utf-8')
-	 	dept_desc = data['dept_desc'].encode('utf-8')
 
-		if idf =='UpdateDepartment':
-			query = session.query(Department)
-			query_updated = query.from_statement("select * from department where dept_code=:dept_code").params(dept_code=dept_code).first() ##
-			if query_updated is None:
-				return jsonify(results="0")
-			query_updated.dept_code = dept_code
-			query_updated.dept_desc = dept_desc
-			# 세션 커밋
-			session.commit()
-			session.close()
-			return jsonify(results = "1")
+	# elif request.method== 'PUT' :
+	# 	data = request.get_json(force=True)
+	# 	idf = data['a'].encode('utf-8')
+	# 	dept_code = data['dept_code'].encode('utf-8')
+	#  	dept_desc = data['dept_desc'].encode('utf-8')
 
-	elif request.method=='DELETE':##
+	# 	if idf =='UpdateDepartment':
+	# 		query = session.query(Department)
+	# 		query_updated = query.from_statement("select * from department where dept_code=:dept_code").params(dept_code=dept_code).first() ##
+	# 		if query_updated is None:
+	# 			return jsonify(results="0")
+	# 		query_updated.dept_code = dept_code
+	# 		query_updated.dept_desc = dept_desc
+	# 		# 세션 커밋
+	# 		session.commit()
+	# 		session.close()
+	# 		return jsonify(results = "1")
+
+	elif request.method=='DELETE':
 		data = request.get_json(force=True)
 		idf = data['a'].encode('utf-8')
 		dept_code = data['dept_code'].encode('utf-8')
@@ -354,6 +383,24 @@ def show_deptobj():
 			session.close()
 			return jsonify(results = "1")
 
+		if idf=='UpdateDeptObj':
+			query = session.query(Dept_obj)
+			query_list = query.from_statement("SELECT * FROM dept_obj WHERE dept_code=:dept_code AND obj_code=:obj_code").params(dept_code=dept_code, obj_code=obj_code).first()
+
+			if query_list is None:
+				return jsonify(results="0")
+############문제해결 org = {'a': 'UpdateDeptObj', 'dept_code':'2', 'obj_code':'2', 'dept_obj_resp':'0', 'dept_obj_auth':'0', 'dept_obj_exp':'1','dept_obj_work':'1','dept_obj_ref':'0'}
+			query_list.dept_code = dept_code
+			query_list.obj_code = obj_code
+			query_list.dept_obj_resp = dept_obj_resp
+			query_list.dept_obj_auth = dept_obj_auth
+			query_list.dept_obj_exp = dept_obj_exp
+			query_list.dept_obj_work = dept_obj_work
+			query_list.dept_obj_ref = dept_obj_ref
+			session.commit()
+			session.close()
+			return jsonify(results ="1")
+
 		elif type(idf) == type("") :
 		  	return jsonify(results = 0)
 
@@ -391,37 +438,7 @@ def show_deptobj():
 	# 		session.commit()
 	# 		session.close()
 	# 		return jsonify(results = "1")
-
-	elif request.method == "PUT":
-		#Get Request
-	 	data = request.get_json(force=True)
-	 	#get data from requested
-	 	idf = data['a'].encode('utf-8')
-	 	dept_code = data['dept_code'].encode('utf-8')
-		obj_code = data['obj_code'].encode('utf-8')
-	 	dept_obj_resp = data['dept_obj_resp'].encode('utf-8') #책임여부
-		dept_obj_auth = data['dept_obj_auth'].encode('utf-8') # 권한여부
-		dept_obj_exp = data['dept_obj_exp'].encode('utf-8') # 경험여부
-		dept_obj_work = data['dept_obj_work'].encode('utf-8') # 작업여부
-		dept_obj_ref = data['dept_obj_ref'].encode('utf-8') #참조여부
-
-		if idf=='UpdateDeptObj':
-			query = session.query(Dept_obj)
-			query_list = query.from_statement("SELECT * FROM dept_obj WHERE dept_code=:dept_code AND obj_code=:obj_code").params(dept_code=dept_code, obj_code=obj_code).first()
-
-			if query_list is None:
-				return jsonify(results="0")
-############문제해결 org = {'a': 'UpdateDeptObj', 'dept_code':'2', 'obj_code':'2', 'dept_obj_resp':'0', 'dept_obj_auth':'0', 'dept_obj_exp':'1','dept_obj_work':'1','dept_obj_ref':'0'}
-			query_list.dept_code = dept_code
-			query_list.obj_code = obj_code
-			query_list.dept_obj_resp = dept_obj_resp
-			query_list.dept_obj_auth = dept_obj_auth
-			query_list.dept_obj_exp = dept_obj_exp
-			query_list.dept_obj_work = dept_obj_work
-			query_list.dept_obj_ref = dept_obj_ref
-			session.commit()
-			session.close()
-			return jsonify(results ="1")
+		
 
 	elif request.method =="DELETE":
 		#Get Request
